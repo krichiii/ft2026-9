a38e4afe5c442f2566d8740c04b96678f6d495b9
Stanislav bc2025 <krichi306@gmail.com>
2026-04-22 14:20:41 +0300

    added train routes data

---
+++ src/data/trains.js
@@ -0,0 +1,47 @@
+export const trains = [
+  {
+    id: 1,
+    number: "705К",
+    route: { from: "Kyiv-Pasazhyrskyi", to: "Lviv" },
+    departure: "2026-04-22T06:00:00",
+    arrival: "2026-04-22T11:30:00",
+    duration: "5h 30m",
+    price: 450
+  },
+  {
+    id: 2,
+    number: "106К",
+    route: { from: "Kyiv-Pasazhyrskyi", to: "Odesa" },
+    departure: "2026-04-22T08:15:00",
+    arrival: "2026-04-22T15:20:00",
+    duration: "7h 05m",
+    price: 520
+  },
+  {
+    id: 3,
+    number: "043К",
+    route: { from: "Lviv", to: "Ivano-Frankivsk" },
+    departure: "2026-04-22T10:45:00",
+    arrival: "2026-04-22T13:10:00",
+    duration: "2h 25m",
+    price: 280
+  },
+  {
+    id: 4,
+    number: "017О",
+    route: { from: "Kharkiv-Pas", to: "Kyiv-Pasazhyrskyi" },
+    departure: "2026-04-22T14:30:00",
+    arrival: "2026-04-22T20:15:00",
+    duration: "5h 45m",
+    price: 390
+  },
+  {
+    id: 5,
+    number: "743К",
+    route: { from: "Dnipro-Holovnyi", to: "Kyiv-Pasazhyrskyi" },
+    departure: "2026-04-22T17:20:00",
+    arrival: "2026-04-22T23:45:00",
+    duration: "6h 25m",
+    price: 480
+  }
+];
